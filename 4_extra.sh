c16736a56048370c426c2a93723869ec48d045a8
arman <arman@helloaida.io>
2017-01-12 17:49:30 -0800

    added extra

---
+++ 4_extra.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get install openssh-server -y
+sudo systemctl restart ssh
+pip install virtualenv
+pip3 install virtualenv
+pip install pandas
+pip3 install pandas
+pip install sklearn
+pip3 install sklearn
+pip install spacy
+pip3 install spacy
+pip install requests
+pip3 install requests
