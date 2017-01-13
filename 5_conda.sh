effdc048ec57916e0fea17a2ed5d9c2f93e2c2c1
arman <arman@helloaida.io>
2017-01-13 12:20:59 -0800

    added miniconda

--- 5_conda.sh
+++ 5_conda.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# After installing anaconda `pip` should be from Anaconda package
+# After installing anaconda, `pip` should be from Anaconda package
+wget -O /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# This will be interactive:
+bash /tmp/miniconda.sh
+source ~/.bashrc
+which conda || exit 1
 conda update conda -y
-conda update anaconda -y
 conda update --all -y
 pip --version | grep 'conda' || exit 1
 pip install tensorflow-gpu
 pip install Theano
 pip install keras
+conda install spacy -y
+conda install pandas -y
+conda install scikit-learn -y
+conda install seaborn -y
+conda install ipython -y
+conda install spyder -y
+conda install jupyter -y
