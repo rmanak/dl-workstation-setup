88be724aa32a4c74d53cc5a267fea32eee3b0fa5
arman <arman@helloaida.io>
2017-01-13 17:40:54 -0800

    added spacy download

--- 4_extra.sh
+++ 4_extra.sh
@@ -4,7 +4,9 @@ sudo pip3 install pandas
 sudo pip install sklearn
 sudo pip3 install sklearn
 sudo pip install spacy
+sudo /usr/bin/python2 -m spacy.en.download
 sudo pip3 install spacy
+sudo /usr/bin/python3 -m spacy.en.download
 sudo pip install requests
 sudo pip3 install requests
 sudo pip install virtualenv
--- 5_conda.sh
+++ 5_conda.sh
@@ -2,7 +2,7 @@
 # After installing anaconda, `pip` should be from Anaconda package
 wget -O /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 # This will be interactive:
-bash /tmp/miniconda.sh
+#bash /tmp/miniconda.sh
 source ~/.bashrc
 which conda || exit 1
 conda update conda -y
@@ -18,3 +18,5 @@ conda install seaborn -y
 conda install ipython -y
 conda install spyder -y
 conda install jupyter -y
+python --version 2>&1 | grep 'Continuum' || exit 1
+python -m spacy.en.download
