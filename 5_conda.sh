50fc475ae9f581277fa1fc2e29b80abbcc810bb5
arman <arman@helloaida.io>
2017-05-04 10:18:25 -0700

    added anaconda path guide

--- 5_conda.sh
+++ 5_conda.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 wget -O /tmp/conda.sh https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash /tmp/conda.sh -b -p $HOME/anaconda
+# Make sure anaconda path is added to your .bashrc file
+# run the following if needed
+# echo "export PATH=$HOME/anaconda/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 which conda || exit 1
 python --version 2>&1 |  grep 'Anaconda' || exit 1
